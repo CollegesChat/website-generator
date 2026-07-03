e736610be13c3e41eeef927979852362d9949c9a
kaesinol <keiplyer@gmail.com>
2026-07-03 21:10:29 +0800

    :bug: bugfix

--- generator/config.py
+++ generator/config.py
@@ -21,7 +21,7 @@ SITE_DIR = Path(os.getenv("SITE_DIR", r"/mnt/data/Project/questionnaire-report-t
 NAME_PREPROCESS = re.compile(r"[\(\)（）【】#]")
 FILENAME_PREPROCESS = re.compile(r"[/>|:&]")
 
-BASE_URL = "https://github.com/CollegesChat/university-information/raw/refs/heads/v2/questionnaires/"
+BASE_URL = "https://github.com/CollegesChat/university-information/raw/refs/heads/v2/"
 DOC_URL = BASE_URL + "docs/choose-a-college/"
 CSV_URL = "https://github.com/CollegesChat/china-university-list/releases/latest/download/output.csv"
 DATA_URL = [
