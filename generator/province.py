344580b1d6a46faaa2880eb89c1cbe89cef24ae2
kaesinol <keiplyer@gmail.com>
2026-08-01 21:18:15 +0800

    :bug: fix province weight

--- generator/province.py
+++ generator/province.py
@@ -29,4 +29,4 @@ def find_province(name: str, mapping: list[tuple[str, str]]) -> str:
         return "国外"
     if not NORMAL_NAME_MATCHER.search(name):
         return "不予收录"
-    return "无效"
+    return "国外"
